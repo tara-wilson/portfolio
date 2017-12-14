ee7d96eaf73b00e6bd52a3810eaa71258eef024f
Tara Wilson <tarawilson@Taras-MacBook-Pro.local>
2017-12-14 13:08:55 -0500

    new files

--- imports/App.js
+++ imports/App.js
@@ -9,10 +9,7 @@ import { createContainer } from 'meteor/react-meteor-data';
 class App extends Component {
   constructor(props) {
     super(props);
-
-
     this.state = {
-
     };
   }
 
@@ -26,28 +23,24 @@ class App extends Component {
         <p>I am a Mobile Developer. Here is some stuff I've created.</p>
       </div>
       <div className="row">
-          <h4><a href="#vid1">Musique Live iOS app</a></h4>
-          <p className="links"><a href="https://github.com/musique-live/iosApp">Github</a><a href="https://www.musiqueliveapp.com">Website</a><a href="https://itunes.apple.com/us/app/musique-live/id1217586564">Download Link</a></p>
-      </div>
-      <div className="row">
-          <h4><a href="#vid7">Musique Live android app</a></h4>
-          <p className="links"><a href="https://github.com/tara-wilson/androidApp">Github</a><a href="https://www.musiqueliveapp.com">Website</a><a href="https://play.google.com/store/apps/details?id=com.musiqueliveapp">Download Link</a></p>
+          <h4><a href="#vid1">Musique Live app</a></h4>
+          <p className="links"><a href="https://github.com/musique-live/iosApp">Github</a><a href="https://www.musiqueliveapp.com">Website</a><a href="https://itunes.apple.com/us/app/musique-live/id1217586564">iOS App</a><a href="https://play.google.com/store/apps/details?id=com.musiqueliveapp">Android App</a></p>
       </div>
       <div className="row">
           <h4><a href="#vid2">Augmented Reality Game</a></h4>
           <p className="links"><a href="https://github.com/tara-wilson/ARMaze">Github</a><a href="https://itunes.apple.com/us/app/md-sunrise-farms-corn-maze/id1289492274?mt=8">Download Link</a></p>
       </div>
       <div className="row">
-          <h4><a href="#vid3">App to turn my lights on with NFC</a></h4>
+          <h4><a href="#vid3">NFC Lights Project</a></h4>
           <p className="links"><a href="https://github.com/tara-wilson/nfc-lights">Github</a></p>
       </div>
        <div className="row">
-        <h4>E-book activity app</h4>
-        <p className="links">App preview coming soon, <a href="http://ryanstewman.com/">Client Website</a></p> 
+        <h4><a href="#vid11">E-book activity app</a></h4>
+        <p className="links">App download coming soon, <a href="http://ryanstewman.com/">Client Website</a></p> 
       </div>
       <div className="row">
-          <h4>Smoochie Poochies</h4>
-          <p className="links">App preview coming soon, <a href="https://www.mysmoochiepoochies.com/">Client Website</a></p> 
+          <h4><a href="#vid10">Smoochie Poochies Scheduler App</a></h4>
+          <p className="links">App download coming soon, <a href="https://www.mysmoochiepoochies.com/">Client Website</a></p> 
      </div>
      </div>
      <div className="outsideright">
@@ -78,8 +71,12 @@ class App extends Component {
           <source src="maze.mp4" type="video/mp4" /></video>
       <video id="vid3" className="video" autoplay="autoplay" loop="loop" width="285" height="515">
         <source src="lights.mp4" type="video/mp4" /></video>
+        <video id="vid10" className="videolarge" autoplay="autoplay" loop="loop" width="285" height="515">
+        <source src="smooch.mp4" type="video/mp4" /></video>
+        <video id="vid11" className="videolarge" autoplay="autoplay" loop="loop" width="285" height="515">
+        <source src="ebook.mp4" type="video/mp4" /></video>
       <img id="vid4" className="image" src='bfimage.png'/>
-      <video id="vid5" className="video" autoplay="autoplay" loop="loop" width="285" height="515">
+      <video id="vid5" className="videolarge" autoplay="autoplay" loop="loop" width="285" height="515">
         <source src="quizchat.mp4" type="video/mp4" /></video>
         <img id="vid6" className="image" src='hutch.png'/>
         <img id="vid8" className="image" src='cornell.png'/>
